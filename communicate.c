3834c2ef0abcfc1dd7b85a32d87fe39a179c2164
Clint Adams <clint@debian.org>
2020-09-22 16:53:31 -0400

    wrap statx().  closes: #940056.

--- communicate.c
+++ communicate.c
@@ -1,6 +1,6 @@
 /*
-  Copyright Ⓒ 1997, 1998, 1999, 2000, 2001  joost witteveen
-  Copyright Ⓒ 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009  Clint Adams
+  Copyright © 1997, 1998, 1999, 2000, 2001  joost witteveen
+  Copyright © 2002-2020  Clint Adams
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
